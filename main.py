ac16e73a47e2d3b53f3e50ca3e285aaab06770d0
Salvador Guzman <guzmansalv@gmail.com>
2020-05-25 08:10:16 -0700

    working solution

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import journal_pb2
+import sys
+import time
+
+
 def main() -> None:
-    print('hello')
+    journal: journal_pb2.JournalEntry = journal_pb2.JournalEntry()
+    journal.time = int(time.time())
+    journal.message = ' '.join(sys.argv[1:])
+
+    print(journal)
+    print(journal.SerializeToString())
 
 
 if __name__ == '__main__':
